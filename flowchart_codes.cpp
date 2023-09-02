51f3072d89c13553a9b611127d2d9c8b04ef9499
pratik10x2 <pratiknikam547@gmail.com>
2023-09-02 15:56:56 +0530

    codes of all flowcharts

---
+++ flowchart_codes.cpp
@@ -0,0 +1,209 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    
+//sum
+    //  int a=5,b=5;
+    //  int sum=a+b;
+    //  cout<<sum<<endl;
+
+//Average
+    // int a=5,b=5,c=5;
+    // int avg=(a+b+c)/3;
+    // cout<<avg;
+
+//Square
+    // int a=2;
+    // int square=a*a;
+    // cout<<square;
+
+//Area
+    // int side=5;
+    // int area=side*side;
+    // cout<<area;
+
+//Percentage
+
+    // int a=50,b=60,c=70,d=80,e=90;
+    // int percentage=((a+b+c+d+e)/5);
+    // cout<<percentage;
+
+//Even or odd
+    // int num;
+    // cout<<"Enter the number:";
+    // cin>>num;
+    // if(num%2==0){
+    //     cout<<"Even";
+    // }
+    // else{
+    //     cout<<"Odd";
+    // }
+
+//Positive ,negative or 0
+    // int num;
+    // cout<<"Enter the number:";
+    // cin>>num;
+    // if(num>0){
+    //     cout<<"Positive";
+    // }
+    // else if(num<0){
+    //     cout<<"Negative";
+    // }
+    // else{
+    //     cout<<"Zero";
+    // }
+
+//FInding grades
+
+    // int marks;
+    // cout<<"Enter the marks:";
+    // cin>>marks;
+    // if(marks>=90){
+    //     cout<<"A grade";
+    // }
+    // else if(marks>=70){
+    //     cout<<"B grade";
+    // }
+    // else if(marks>=50){
+    //     cout<<"C grade";
+    // }
+    // else{
+    //     cout<<"D grade";
+    // }
+
+//Print  1 to n
+    // int n;
+    // cout<<"Enter the number:";
+    // cin>>n;
+    // for(int i=0;i<n;i++){
+    //     cout<<i<<endl;
+    // }
+
+//Print n to 1
+    // int n;
+    // cout<<"Enter the number:";
+    // cin>>n;
+    // for(int i=n;i>0;i--){
+    //     cout<<i<<endl;
+    // }
+
+//Take numbers from user and print numbers
+    // int num;
+    // cout << "Enter the number:";
+    // cin >> num;
+    // int n;
+
+    // for (int i = 1; i <= num; i++)
+    // {
+    //     cout << "plz Enter the value:"<<endl;
+    //     cin>>n;
+    //     cout<<"The value is:";
+    // cout << n<<endl;
+    // }
+
+//Multiply N numbers from users and print number
+    // int num, n;
+    // cout << "Enter the number:";
+    // cin >> num;
+    // int ans = 1;
+    // for (int i = 0; i < num; i++)
+    // {
+    //     cout << "Enter the value " << i << ":";
+    //     cin >> n;
+    //     ans = ans * n;
+    // }
+    // cout << ans;
+
+//Print 1 to n , but only even number
+    //     int num;
+    //     cout << "Enter the number:";
+    //     cin >> num;
+    //    for(int i=0;i<num;i++){     //there is a another way -jUst increment i=i+2
+    //     if(i%2==0){
+    //         cout<<i<<endl;
+    //     }
+    //    }
+
+// Calculate the simple interest
+    // int p,r,t;
+    // int si;
+    // cout<<"Enter the Principal amount:";
+    // cin>>p;
+    // cout<<"Enter the Rate:";
+    // cin>>r;
+    // cout<<"Enter the Time:";
+    // cin>>t;
+
+    // si=(p*r*t)/100;
+    // cout<<si;
+
+
+// Calculate the compound interest
+    // double p, r, t;
+    // double amount;
+    // cout << "Enter the Principal amount:";
+    // cin >> p;
+    // cout << "Enter the Rate:";
+    // cin >> r;
+    // cout << "Enter the Time:";
+    // cin >> t;
+
+    // amount=p*(pow((1+r/100),t));
+    // cout<<"The amout is generated:"<<amount<<endl;
+    // int ci=amount-p;
+    // cout<<"The compound interest is:"<<ci;
+
+
+//Factorial of given number
+    // int num;
+    // cout<<"enter the number:";
+    // cin>>num;
+    // int ans=1;
+    // for(int i=num;i>0;i--){
+    //     ans=ans*i;
+
+    // }
+    // cout<<ans;
+
+//Check prime or not
+    // int num;
+    // cout<<"enter the number:";
+    // cin>>num;
+    // int c=0;
+
+    // for( int i=2;i<num;i++)
+    // {
+    //     if(num%i==0){
+    //         c++;
+    //         break;
+    //     }
+       
+    // }
+    // if(c==0){
+    //     cout<<"Prime";
+    // }
+    // else{
+    //     cout<<" No Prime";
+        
+    // }
+
+//Check max of 3 numbers
+    // int a,b,c;
+    // cin>>a>>b>>c;
+
+    // if(a>b && a>c){
+    //     cout<<a;
+    // }
+    // else if(b>c && b>a){
+    //     cout<<b;
+
+    // }
+    // else{
+    //     cout<<c;
+    // }
+   
+    return 0;
+}
