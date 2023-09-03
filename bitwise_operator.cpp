6030c105904d5ea29528fa42ddee84097cd4142b
pratik10x2 <pratiknikam547@gmail.com>
2023-09-03 18:18:51 +0530

    Bitwise ,shift,increment ,decrement operators

---
+++ bitwise_operator.cpp
@@ -0,0 +1,106 @@
+ #include<iostream>
+using namespace std;
+
+int main() {
+
+//Bitwise operators
+    // int a=5;
+    // int b=10;
+    // cout<<(a & b)<<endl;
+    // cout<<(a | b)<<endl;
+
+    // int num=1;
+    // cout<<~num;
+
+    // int a=5;
+    // int b=-5;
+    // cout<<(a^b);
+
+//Left shift And right shift operator
+    // int a=7;
+    // cout<<(a<<1);
+
+//when -ve signed int ,it was handled by compiler 
+//when -ve unsigned int ,it gives big +ve number 
+
+    // int n=-5;
+    // cout<<(n>>1);
+
+//When we left shift +ve number by -1,it gives the garbage value
+    // int n=5;
+    // cout<<(n<<-1);
+
+    
+//Pre and post increment 
+    // int a=8;
+    // cout<<(++a);
+    // cout<<(a++)<<endl;
+    // cout<<a;
+
+//Pre and post decrement 
+    // int a=10;
+    // cout<<(--a)*10;
+    // int n=10;
+    // cout<<(n--)*10;
+
+    // int a=21;
+    // cout<<(++a);
+    // cout<<(a++);
+    // cout<<a;
+
+    // int a=10;
+    // cout<<(++a)*10<<endl;
+    // cout<<(a++)*10<<endl;
+    // cout<<a;
+
+//IN vs code it gives 132
+
+    // int a=10;
+    // cout<<((++a)*(a++));
+
+//IN vs code it gives 120
+    // int n=10;
+    // cout<<((n++)*(++n));
+
+
+//Break and continue 
+
+    // for( int i=0;i<5;i++){
+    //     cout<<i<<endl;
+    //     break;
+    // }
+    // for( int i=0;i<5;i++){
+    //     cout<<i<<endl;
+    //     if(i==100)
+    //     break;
+    // }
+    // for( int i=0;i<5;i++){
+    //     cout<<i<<endl;
+    //     if(i==3)
+    //     break;
+    // }
+
+    // for(int i=0;i<=5;i++){
+    //     cout<<"hello ji"<<endl;
+    //     continue;
+    //     cout<<"Kaise ho sare";
+    // }
+
+
+//Local variable
+    // for(int i=0;i<5;i++){
+    //     cout<<i;
+    // }
+    // cout<<i;  
+
+    // if(true){
+    //     int a=101;
+    //     if(true){
+    //         int a=202;
+    //         cout<<a;
+    //     }
+    // }
+
+return 0;
+    
+}
