b28b744ee18d567dee0780a5f9c87586e63882b9
pratik10x2 <pratiknikam547@gmail.com>
2023-09-04 22:50:32 +0530

    All functions that takes in a class

---
+++ function.cpp
@@ -0,0 +1,164 @@
+#include<iostream>
+using namespace std;
+
+//Print "Sundar ko sundari pasand h"  5 times
+    // void printline(){
+    //     for(int i=0;i<5;i++){
+    //         cout<<"Sundar ko sundari pasand h"<<endl;
+    //     }
+    // }
+    // int main(){
+    //     printline();
+    //     return 0;
+    // }
+
+//to print sum of 3
+    // void printsum(int num1, int num2, int num3){
+    //     int ans=num1+num2+num3;
+    //     cout<<ans;
+    // }
+    // int main(){
+    //     printsum(1,2,3);
+    //     return 0;
+    // }
+
+//to return the sum of 3
+    // int printsum(int num1, int num2, int num3){
+    //     int ans=num1+num2+num3;
+    //     return ans;
+    
+    // }
+    // int main(){
+    //     // int sum=printsum(1,2,3);
+    //     // cout<<sum;
+    //     cout<<printsum(1,2,3);
+    //     return 0;
+    // }
+
+// return is used in void function
+    // void msg(){
+    //     cout<<"msg1";
+    //     return;      //it print only msg1 ,also used for break the loop
+
+    //     cout<<"msg2";
+    // }
+    // int main(){
+    //     msg();
+    //     return 0;
+    // }
+
+//find maximum  of 3
+    // void printMaximum(int num1,int num2,int num3){
+    //     if(num1>num2 && num1>num3){
+    //         cout<<num1;
+    //     }
+    //     else if(num2>num1 && num2>num3){
+    //         cout<<num2;
+    //     }
+    //     else{
+    //         cout<<num3;
+    //     }
+
+    // }
+    // int main(){
+    //     printMaximum(100,22,445);
+
+    //     return 0;
+    // }
+
+//Using inbuilt function
+    // int printmax(int n1,int n2,int n3){
+    //     int ans1=max(n1,n2);
+    //     int ans2=max(ans1,n3);
+    //     return ans2;
+    // }
+    // int main(){
+    //     int max=printmax(100,20,200);
+    //     cout<<max;
+    //     return 0;
+    // }
+
+//counting from 1 to n
+    // void printcount(int n){
+    //     for(int i=1;i<=n;i++){
+    //         cout<<i<<endl;
+    //     }
+    // }
+    
+    // int main(){
+    //     printcount(5);
+    //     return 0;
+    // }
+
+//even  or odd
+    // void checkevenodd(int num){
+    //     if(num%2==0){
+    //         cout<<"Even";
+    //     }
+    //     else{
+    //           cout<<"odd";
+    //     }
+    // }
+    // int main(){
+    //     checkevenodd(5);
+    //     return 0;
+    // }
+
+//Finf sum upto n
+    // void sumupton(int num){
+    //     int sum=0;
+    //     for(int i=0;i<num;i++){
+    //         sum=sum+i;
+    //         //sum+=i;
+    //     }
+    //     cout<<sum;
+
+    // }
+    // int main(){
+    //     sumupton(10);
+    //     return 0;
+
+    // }
+
+//find sum of even numbers
+    // void findsumofeven(int n){
+    //     int sum=0;
+    //     for(int i=2;i<=n;i++){
+    //         if(i%2==0){
+
+    //         sum+=i;
+    //         }
+
+    //     }
+    //     cout<<sum;
+    // }
+    // int main(){
+    //     findsumofeven(7);
+
+    //     return 0;
+    // }
+
+//Check prime or not
+    // bool checkprime(int num){
+
+    //     for(int i=2;i<num;i++){
+    //         if (num%i==0){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+
+    // }
+
+    // int main(){
+    //     bool prime=checkprime(9);
+    //     if(prime){
+    //         cout<<"It's a prime number";
+    //     }
+    //     else{
+    //         cout<<"It's not a prime number";
+
+    //     }
+        
+    //     return 0;
+    // }
