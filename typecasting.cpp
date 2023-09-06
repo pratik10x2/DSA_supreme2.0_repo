a0d0d74f2f3b20735a26e61d1a40375ca6611282
pratik10x2 <pratiknikam547@gmail.com>
2023-09-06 08:23:25 +0530

    All typecasting code

---
+++ typecasting.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+//implicit type conversion
+
+//int to float and float to int
+    // int num1=10;
+    // float num2=5.5;
+    // float result= num1+num2;
+    // int result= num1+num2;
+    // cout<<result;
+
+//char to int 
+    // char ch='A';
+    // int a=ch+1;
+    // cout<<a;
+
+//int to char
+    // int a=97;
+    // char ch=a;
+    // cout<<ch;
+
+//explicit type conversion
+    // int num1=10;
+    // float num2=5.5;
+    // float a=num1+(int)num2;
+    // cout<<a;
+
+//double to int
+    // double pi=3.1487989;
+    // int intpi=(int(pi));
+    // cout<<intpi;
+
+//float to char
+    // float num1=65.43;
+    // char charvalue=(char)num1;
+
+    // cout<<charvalue;
+
+//int to float
+    // int a=10;
+    // int b=3.0;
+    // float c=a/(float)b;
+    // float c=(float)a/b;
+    // cout<<c;
+
+//float to int
+    // float a=10;
+    // float b=3.0;
+    // int c=(float)a/b;
+    // cout<<c;
+
+    return 0;
+
+}
