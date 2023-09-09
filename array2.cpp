56e0171252c4fab898e27f9ee8db6d3d8a9c3325
pratik10x2 <pratiknikam547@gmail.com>
2023-09-09 21:56:59 +0530

    finding unique elment ,two pairs,three pairs

---
+++ array2.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+
+
+//Find the unique element
+    // int getunique(int arr[],int n){
+    //     int ans=0;
+    //     for(int i=0;i<n;i++){
+    //         ans=ans^arr[i];
+    //     }
+    //     return ans;
+    // }
+
+
+    // int main(){
+    //     int arr[]={2,3,2,3,10};
+    //     int n=5;
+    //     int finalans=getunique(arr,n);
+    //     cout<<finalans;
+    
+    //     return 0;
+    // }
+
+//print all pairs
+    // int main(){
+    //     int arr[]={10,20,30};
+    //     int n=3;
+    //     for(int i=0;i<n;i++){
+    //         for(int j=0;j<n;j++){
+    //             // cout<<i<<j<<endl;
+    //             cout<<"("<<arr[i]<<","<<arr[j]<<")"<<endl;
+    //         }
+    //     }
+    //     return 0;
+    // }
+
+
+//print triplets 
+    // int main(){
+    //     int arr[]={10,20,30};
+    //     int n=3;
+    //     for(int i=0;i<n;i++){
+    //         for(int j=0;j<n;j++){
+    //             // cout<<i<<j<<endl;
+    //             for(int k=0;k<n;k++){
+
+    //             cout<<"("<<arr[i]<<","<<arr[j]<<","<<arr[k]<<")"<<endl;
+    //             }
+    //         }
+    //     }
+    //     return 0;
+    // }
+
+//The addition of each triplet
+    // int main(){
+    //     int arr[]={10,20,30};
+    //     int n=3;
+    //     for(int i=0;i<n;i++){
+    //         for(int j=0;j<n;j++){
+    //             // cout<<i<<j<<endl;
+    //             for(int k=0;k<n;k++){
+
+    //             cout<<"("<<arr[i]<<","<<arr[j]<<","<<arr[k]<<")"<<"=";
+    //             int ans=arr[i]+arr[j]+arr[k];
+    //             cout<<ans<<endl;
+    //             }
+    //         }
+    //     }
+    //     return 0;
+    // }
