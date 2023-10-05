057a833cb0527c92e7a7e8cf1bbd06a47ee071a1
pratik10x2 <pratiknikam547@gmail.com>
2023-10-05 21:26:01 +0530

    Integer and char pointers

---
+++ Week 6/pointer.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+//     int a =100;
+//     int *ptr= &a;
+//   cout<<sizeof(ptr)<<endl;
+
+//   char vh='k';
+//   char *cptr=&vh;                  // it gives a same ans of all three is 4
+//   cout<<sizeof(cptr)<<endl;
+
+//   long b=10;
+//   long *lptr=&b;
+//   cout<<sizeof(lptr)<<endl;
+
+// int a=100;
+// int *ptr=&a; 
+// a=a+1;   //101
+// *ptr==*ptr+1;      //101
+// cout<<a<<" "<<*ptr;
+
+// int a=100;
+// int *ptr=&a;
+// cout<<a<<endl;
+// cout<<&a<<endl;
+// // cout<<*a<<endl;//it will give an error
+// cout<<ptr<<endl;
+// cout<<&ptr<<endl;
+// cout<<*ptr<<endl;
+// cout<<(*ptr++)<<endl;
+// cout<<++(*ptr)<<endl;
+// cout<<*ptr/2<<endl;
+
+// int arr[5]={10,20,30,40,50};
+
+// cout<<arr<<endl;
+// cout<<&arr<<endl;
+// cout<<*arr<<endl;
+// cout<<*arr+1<<endl;
+// cout<<*(arr)+1<<endl;
+// cout<<*(arr+2)<<endl;
+// cout<<*(arr+3)<<endl;
+
+
+// int arr[4]={10,20,30,40};
+// int *p=arr;
+// int *q=arr+1;
+
+// cout<<arr<<endl;
+// cout<<&arr<<endl;
+// cout<<arr[0]<<endl;
+// cout<<&arr[0]<<endl;
+// cout<<p<<endl;
+// cout<<&p<<endl;
+// cout<<*p<<endl;
+// cout<<q<<endl;
+// cout<<&q<<endl;
+// cout<<*q<<endl;
+// cout<<*p+1<<endl;
+// cout<<*(p)+2<<endl;
+// cout<<*(q)+2<<endl;
+// cout<<*(q+4)<<endl;   //it will give garbage value or error
+
+
+// char ch[50]="pratik";
+// char *cptr=ch;
+// cout<<*cptr<<endl;  //it will print first alphabet 
+// cout<<cptr;      //it will print all word
+
+// char ch[50]="statement";
+// char *cptr=ch;
+// cout<<ch<<endl;   
+// cout<<&ch<<endl;   
+// cout<<*ch<<endl;   
+// cout<<*(ch+3)<<endl;   
+// cout<<cptr<<endl;   
+// cout<<*cptr<<endl;   
+// cout<<&cptr<<endl;   
+// cout<<*(cptr+3)<<endl;   
+// cout<<(cptr+2)<<endl;   
+// cout<<(cptr+8)<<endl;   
+
+    return 0;
+}
